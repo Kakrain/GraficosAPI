5afff0b2c347236302518a3b9132d4a053d25739
Kakrain <wherethesunnoshine@hotmail.com>
2014-12-08 03:34:40 -0500

    Commit Inicial
    Finalmente... reconoce patrones de:
    rectangulo
    circulo
    triangulo
    zigzag
    x y cancel (que son muy similares)
    y estaba intentando añadir el de espiral... sin mucho exito

---
+++ dollar.js
@@ -0,0 +1,343 @@
+/**
+ * The $1 Unistroke Recognizer (JavaScript version)
+ *
+ *	Jacob O. Wobbrock, Ph.D.
+ * 	The Information School
+ *	University of Washington
+ *	Seattle, WA 98195-2840
+ *	wobbrock@uw.edu
+ *
+ *	Andrew D. Wilson, Ph.D.
+ *	Microsoft Research
+ *	One Microsoft Way
+ *	Redmond, WA 98052
+ *	awilson@microsoft.com
+ *
+ *	Yang Li, Ph.D.
+ *	Department of Computer Science and Engineering
+ * 	University of Washington
+ *	Seattle, WA 98195-2840
+ * 	yangli@cs.washington.edu
+ *
+ * The academic publication for the $1 recognizer, and what should be 
+ * used to cite it, is:
+ *
+ *	Wobbrock, J.O., Wilson, A.D. and Li, Y. (2007). Gestures without 
+ *	  libraries, toolkits or training: A $1 recognizer for user interface 
+ *	  prototypes. Proceedings of the ACM Symposium on User Interface 
+ *	  Software and Technology (UIST '07). Newport, Rhode Island (October 
+ *	  7-10, 2007). New York: ACM Press, pp. 159-168.
+ *
+ * The Protractor enhancement was separately published by Yang Li and programmed 
+ * here by Jacob O. Wobbrock:
+ *
+ *	Li, Y. (2010). Protractor: A fast and accurate gesture
+ *	  recognizer. Proceedings of the ACM Conference on Human
+ *	  Factors in Computing Systems (CHI '10). Atlanta, Georgia
+ *	  (April 10-15, 2010). New York: ACM Press, pp. 2169-2172.
+ *
+ * This software is distributed under the "New BSD License" agreement:
+ *
+ * Copyright (C) 2007-2012, Jacob O. Wobbrock, Andrew D. Wilson and Yang Li.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the names of the University of Washington nor Microsoft,
+ *      nor the names of its contributors may be used to endorse or promote
+ *      products derived from this software without specific prior written
+ *      permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Jacob O. Wobbrock OR Andrew D. Wilson
+ * OR Yang Li BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**/
+//
+// Point class
+//
+function Point(x, y) // constructor
+{
+	this.X = x;
+	this.Y = y;
+}
+//
+// Rectangle class
+//
+function Rectangle(x, y, width, height) // constructor
+{
+	this.X = x;
+	this.Y = y;
+	this.Width = width;
+	this.Height = height;
+}
+//
+// Unistroke class: a unistroke template
+//
+function Unistroke(name, points) // constructor
+{
+	this.Name = name;
+	this.Points = Resample(points, NumPoints);
+	var radians = IndicativeAngle(this.Points);
+	this.Points = RotateBy(this.Points, -radians);
+	this.Points = ScaleTo(this.Points, SquareSize);
+	this.Points = TranslateTo(this.Points, Origin);
+	this.Vector = Vectorize(this.Points); // for Protractor
+}
+//
+// Result class
+//
+function Result(name, score) // constructor
+{
+	this.Name = name;
+	this.Score = score;
+}
+//
+// DollarRecognizer class constants
+//
+var NumUnistrokes = 7;
+var NumPoints = 64;
+var SquareSize = 250.0;
+var Origin = new Point(0,0);
+var Diagonal = Math.sqrt(SquareSize * SquareSize + SquareSize * SquareSize);
+var HalfDiagonal = 0.5 * Diagonal;
+var AngleRange = Deg2Rad(45.0);
+var AnglePrecision = Deg2Rad(2.0);
+var Phi = 0.5 * (-1.0 + Math.sqrt(5.0)); // Golden Ratio
+//
+// DollarRecognizer class
+//
+function DollarRecognizer() // constructor
+{
+	//
+	// one built-in unistroke per gesture type
+	//
+	this.Unistrokes = new Array(NumUnistrokes);
+	this.Unistrokes[0] = new Unistroke("triangle", new Array(new Point(137,139),new Point(135,141),new Point(133,144),new Point(132,146),new Point(130,149),new Point(128,151),new Point(126,155),new Point(123,160),new Point(120,166),new Point(116,171),new Point(112,177),new Point(107,183),new Point(102,188),new Point(100,191),new Point(95,195),new Point(90,199),new Point(86,203),new Point(82,206),new Point(80,209),new Point(75,213),new Point(73,213),new Point(70,216),new Point(67,219),new Point(64,221),new Point(61,223),new Point(60,225),new Point(62,226),new Point(65,225),new Point(67,226),new Point(74,226),new Point(77,227),new Point(85,229),new Point(91,230),new Point(99,231),new Point(108,232),new Point(116,233),new Point(125,233),new Point(134,234),new Point(145,233),new Point(153,232),new Point(160,233),new Point(170,234),new Point(177,235),new Point(179,236),new Point(186,237),new Point(193,238),new Point(198,239),new Point(200,237),new Point(202,239),new Point(204,238),new Point(206,234),new Point(205,230),new Point(202,222),new Point(197,216),new Point(192,207),new Point(186,198),new Point(179,189),new Point(174,183),new Point(170,178),new Point(164,171),new Point(161,168),new Point(154,160),new Point(148,155),new Point(143,150),new Point(138,148),new Point(136,148)));
+	this.Unistrokes[1] = new Unistroke("x", new Array(new Point(87,142),new Point(89,145),new Point(91,148),new Point(93,151),new Point(96,155),new Point(98,157),new Point(100,160),new Point(102,162),new Point(106,167),new Point(108,169),new Point(110,171),new Point(115,177),new Point(119,183),new Point(123,189),new Point(127,193),new Point(129,196),new Point(133,200),new Point(137,206),new Point(140,209),new Point(143,212),new Point(146,215),new Point(151,220),new Point(153,222),new Point(155,223),new Point(157,225),new Point(158,223),new Point(157,218),new Point(155,211),new Point(154,208),new Point(152,200),new Point(150,189),new Point(148,179),new Point(147,170),new Point(147,158),new Point(147,148),new Point(147,141),new Point(147,136),new Point(144,135),new Point(142,137),new Point(140,139),new Point(135,145),new Point(131,152),new Point(124,163),new Point(116,177),new Point(108,191),new Point(100,206),new Point(94,217),new Point(91,222),new Point(89,225),new Point(87,226),new Point(87,224)));
+	this.Unistrokes[2] = new Unistroke("rectangle", new Array(new Point(78,149),new Point(78,153),new Point(78,157),new Point(78,160),new Point(79,162),new Point(79,164),new Point(79,167),new Point(79,169),new Point(79,173),new Point(79,178),new Point(79,183),new Point(80,189),new Point(80,193),new Point(80,198),new Point(80,202),new Point(81,208),new Point(81,210),new Point(81,216),new Point(82,222),new Point(82,224),new Point(82,227),new Point(83,229),new Point(83,231),new Point(85,230),new Point(88,232),new Point(90,233),new Point(92,232),new Point(94,233),new Point(99,232),new Point(102,233),new Point(106,233),new Point(109,234),new Point(117,235),new Point(123,236),new Point(126,236),new Point(135,237),new Point(142,238),new Point(145,238),new Point(152,238),new Point(154,239),new Point(165,238),new Point(174,237),new Point(179,236),new Point(186,235),new Point(191,235),new Point(195,233),new Point(197,233),new Point(200,233),new Point(201,235),new Point(201,233),new Point(199,231),new Point(198,226),new Point(198,220),new Point(196,207),new Point(195,195),new Point(195,181),new Point(195,173),new Point(195,163),new Point(194,155),new Point(192,145),new Point(192,143),new Point(192,138),new Point(191,135),new Point(191,133),new Point(191,130),new Point(190,128),new Point(188,129),new Point(186,129),new Point(181,132),new Point(173,131),new Point(162,131),new Point(151,132),new Point(149,132),new Point(138,132),new Point(136,132),new Point(122,131),new Point(120,131),new Point(109,130),new Point(107,130),new Point(90,132),new Point(81,133),new Point(76,133)));
+	this.Unistrokes[3] = new Unistroke("circle", new Array(new Point(127,141),new Point(124,140),new Point(120,139),new Point(118,139),new Point(116,139),new Point(111,140),new Point(109,141),new Point(104,144),new Point(100,147),new Point(96,152),new Point(93,157),new Point(90,163),new Point(87,169),new Point(85,175),new Point(83,181),new Point(82,190),new Point(82,195),new Point(83,200),new Point(84,205),new Point(88,213),new Point(91,216),new Point(96,219),new Point(103,222),new Point(108,224),new Point(111,224),new Point(120,224),new Point(133,223),new Point(142,222),new Point(152,218),new Point(160,214),new Point(167,210),new Point(173,204),new Point(178,198),new Point(179,196),new Point(182,188),new Point(182,177),new Point(178,167),new Point(170,150),new Point(163,138),new Point(152,130),new Point(143,129),new Point(140,131),new Point(129,136),new Point(126,139)));
+	this.Unistrokes[4] = new Unistroke("zig-zag", new Array(new Point(307,216),new Point(333,186),new Point(356,215),new Point(375,186),new Point(399,216),new Point(418,186)));
+	this.Unistrokes[5] = new Unistroke("delete", new Array(new Point(123,129),new Point(123,131),new Point(124,133),new Point(125,136),new Point(127,140),new Point(129,142),new Point(133,148),new Point(137,154),new Point(143,158),new Point(145,161),new Point(148,164),new Point(153,170),new Point(158,176),new Point(160,178),new Point(164,183),new Point(168,188),new Point(171,191),new Point(175,196),new Point(178,200),new Point(180,202),new Point(181,205),new Point(184,208),new Point(186,210),new Point(187,213),new Point(188,215),new Point(186,212),new Point(183,211),new Point(177,208),new Point(169,206),new Point(162,205),new Point(154,207),new Point(145,209),new Point(137,210),new Point(129,214),new Point(122,217),new Point(118,218),new Point(111,221),new Point(109,222),new Point(110,219),new Point(112,217),new Point(118,209),new Point(120,207),new Point(128,196),new Point(135,187),new Point(138,183),new Point(148,167),new Point(157,153),new Point(163,145),new Point(165,142),new Point(172,133),new Point(177,127),new Point(179,127),new Point(180,125)));
+	this.Unistrokes[6] =new Unistroke("spiral",new Array(new Point(372,286),new Point(372,289),new Point(372,292),new Point(372,297),new Point(371,307),new Point(368,314),new Point(364,315),new Point(358,313),new Point(348,309),new Point(340,303),new Point(336,299),new Point(335,294),new Point(335,289),new Point(335,285),new Point(336,278),new Point(337,266),new Point(340,254),new Point(346,246),new Point(353,237),new Point(361,233),new Point(369,233),new Point(380,233),new Point(398,236),new Point(409,237),new Point(419,241),new Point(432,248),new Point(442,256),new Point(451,265),new Point(462,281),new Point(467,295),new Point(471,319),new Point(470,335),new Point(465,355),new Point(460,368),new Point(449,387),new Point(443,397),new Point(433,414),new Point(423,428),new Point(414,436),new Point(402,443),new Point(390,446),new Point(366,447),new Point(351,446),new Point(329,440),new Point(315,434),new Point(301,425),new Point(285,412),new Point(276,401),new Point(264,383),new Point(256,368),new Point(247,343),new Point(241,321),new Point(238,304),new Point(233,280),new Point(232,264),new Point(232,243),new Point(233,229),new Point(238,204),new Point(245,188),new Point(259,165),new Point(270,154),new Point(284,143),new Point(297,138),new Point(323,131),new Point(339,127),new Point(365,123),new Point(386,121),new Point(409,120),new Point(444,120),new Point(467,120),new Point(488,121),new Point(511,126),new Point(522,130),new Point(537,140),new Point(553,153),new Point(571,172),new Point(592,210),new Point(600,232),new Point(606,255),new Point(613,289),new Point(615,314),new Point(616,336),new Point(614,367),new Point(610,391),new Point(604,424),new Point(597,445),new Point(583,474),new Point(572,490),new Point(554,507),new Point(532,529),new Point(514,542),new Point(495,557),new Point(471,575),new Point(457,586),new Point(433,598),new Point(417,602),new Point(393,603),new Point(372,602),new Point(351,598),new Point(317,590),new Point(294,582),new Point(275,574),new Point(247,559),new Point(230,548),new Point(213,535),new Point(185,512),new Point(169,497),new Point(154,480),new Point(134,455),new Point(123,439),new Point(115,421),new Point(104,398),new Point(100,382),new Point(94,358),new Point(92,345),new Point(91,327),new Point(91,310),new Point(91,299),new Point(92,282),new Point(92,271),new Point(95,260),new Point(98,247),new Point(101,235),new Point(104,228),new Point(109,216),new Point(115,202),new Point(119,192),new Point(125,178),new Point(133,164),new Point(141,153),new Point(148,144),new Point(156,138),new Point(161,133),new Point(170,128),new Point(181,123),new Point(188,119),new Point(191,118)));
+	
+
+
+//
+	// The $1 Gesture Recognizer API begins here -- 3 methods: Recognize(), AddGesture(), and DeleteUserGestures()
+	//
+	this.Recognize = function(points, useProtractor)
+	{
+		points = Resample(points, NumPoints);
+		var radians = IndicativeAngle(points);
+		points = RotateBy(points, -radians);
+		points = ScaleTo(points, SquareSize);
+		points = TranslateTo(points, Origin);
+		var vector = Vectorize(points); // for Protractor
+
+		var b = +Infinity;
+		var u = -1;
+		for (var i = 0; i < this.Unistrokes.length; i++) // for each unistroke
+		{
+			var d;
+			if (useProtractor) // for Protractor
+				d = OptimalCosineDistance(this.Unistrokes[i].Vector, vector);
+			else // Golden Section Search (original $1)
+				d = DistanceAtBestAngle(points, this.Unistrokes[i], -AngleRange, +AngleRange, AnglePrecision);
+			if (d < b) {
+				b = d; // best (least) distance
+				u = i; // unistroke
+			}
+		}
+		return (u == -1) ? new Result("No match.", 0.0) : new Result(this.Unistrokes[u].Name, useProtractor ? 1.0 / b : 1.0 - b / HalfDiagonal);
+	};
+	this.AddGesture = function(name, points)
+	{
+		this.Unistrokes[this.Unistrokes.length] = new Unistroke(name, points); // append new unistroke
+		var num = 0;
+		for (var i = 0; i < this.Unistrokes.length; i++) {
+			if (this.Unistrokes[i].Name == name)
+				num++;
+		}
+		return num;
+	}
+	this.DeleteUserGestures = function()
+	{
+		this.Unistrokes.length = NumUnistrokes; // clear any beyond the original set
+		return NumUnistrokes;
+	}
+}
+//
+// Private helper functions from this point down
+//
+function Resample(points, n)
+{
+	var I = PathLength(points) / (n - 1); // interval length
+	var D = 0.0;
+	var newpoints = new Array(points[0]);
+	for (var i = 1; i < points.length; i++)
+	{
+		var d = Distance(points[i - 1], points[i]);
+		if ((D + d) >= I)
+		{
+			var qx = points[i - 1].X + ((I - D) / d) * (points[i].X - points[i - 1].X);
+			var qy = points[i - 1].Y + ((I - D) / d) * (points[i].Y - points[i - 1].Y);
+			var q = new Point(qx, qy);
+			newpoints[newpoints.length] = q; // append new point 'q'
+			points.splice(i, 0, q); // insert 'q' at position i in points s.t. 'q' will be the next i
+			D = 0.0;
+		}
+		else D += d;
+	}
+	if (newpoints.length == n - 1) // somtimes we fall a rounding-error short of adding the last point, so add it if so
+		newpoints[newpoints.length] = new Point(points[points.length - 1].X, points[points.length - 1].Y);
+	return newpoints;
+}
+function IndicativeAngle(points)
+{
+	var c = Centroid(points);
+	return Math.atan2(c.Y - points[0].Y, c.X - points[0].X);
+}
+function RotateBy(points, radians) // rotates points around centroid
+{
+	var c = Centroid(points);
+	var cos = Math.cos(radians);
+	var sin = Math.sin(radians);
+	var newpoints = new Array();
+	for (var i = 0; i < points.length; i++) {
+		var qx = (points[i].X - c.X) * cos - (points[i].Y - c.Y) * sin + c.X
+		var qy = (points[i].X - c.X) * sin + (points[i].Y - c.Y) * cos + c.Y;
+		newpoints[newpoints.length] = new Point(qx, qy);
+	}
+	return newpoints;
+}
+function ScaleTo(points, size) // non-uniform scale; assumes 2D gestures (i.e., no lines)
+{
+	var B = BoundingBox(points);
+	var newpoints = new Array();
+	for (var i = 0; i < points.length; i++) {
+		var qx = points[i].X * (size / B.Width);
+		var qy = points[i].Y * (size / B.Height);
+		newpoints[newpoints.length] = new Point(qx, qy);
+	}
+	return newpoints;
+}
+function TranslateTo(points, pt) // translates points' centroid
+{
+	var c = Centroid(points);
+	var newpoints = new Array();
+	for (var i = 0; i < points.length; i++) {
+		var qx = points[i].X + pt.X - c.X;
+		var qy = points[i].Y + pt.Y - c.Y;
+		newpoints[newpoints.length] = new Point(qx, qy);
+	}
+	return newpoints;
+}
+function Vectorize(points) // for Protractor
+{
+	var sum = 0.0;
+	var vector = new Array();
+	for (var i = 0; i < points.length; i++) {
+		vector[vector.length] = points[i].X;
+		vector[vector.length] = points[i].Y;
+		sum += points[i].X * points[i].X + points[i].Y * points[i].Y;
+	}
+	var magnitude = Math.sqrt(sum);
+	for (var i = 0; i < vector.length; i++)
+		vector[i] /= magnitude;
+	return vector;
+}
+function OptimalCosineDistance(v1, v2) // for Protractor
+{
+	var a = 0.0;
+	var b = 0.0;
+	for (var i = 0; i < v1.length; i += 2) {
+		a += v1[i] * v2[i] + v1[i + 1] * v2[i + 1];
+                b += v1[i] * v2[i + 1] - v1[i + 1] * v2[i];
+	}
+	var angle = Math.atan(b / a);
+	return Math.acos(a * Math.cos(angle) + b * Math.sin(angle));
+}
+function DistanceAtBestAngle(points, T, a, b, threshold)
+{
+	var x1 = Phi * a + (1.0 - Phi) * b;
+	var f1 = DistanceAtAngle(points, T, x1);
+	var x2 = (1.0 - Phi) * a + Phi * b;
+	var f2 = DistanceAtAngle(points, T, x2);
+	while (Math.abs(b - a) > threshold)
+	{
+		if (f1 < f2) {
+			b = x2;
+			x2 = x1;
+			f2 = f1;
+			x1 = Phi * a + (1.0 - Phi) * b;
+			f1 = DistanceAtAngle(points, T, x1);
+		} else {
+			a = x1;
+			x1 = x2;
+			f1 = f2;
+			x2 = (1.0 - Phi) * a + Phi * b;
+			f2 = DistanceAtAngle(points, T, x2);
+		}
+	}
+	return Math.min(f1, f2);
+}
+function DistanceAtAngle(points, T, radians)
+{
+	var newpoints = RotateBy(points, radians);
+	return PathDistance(newpoints, T.Points);
+}
+function Centroid(points)
+{
+	var x = 0.0, y = 0.0;
+	for (var i = 0; i < points.length; i++) {
+		x += points[i].X;
+		y += points[i].Y;
+	}
+	x /= points.length;
+	y /= points.length;
+	return new Point(x, y);
+}
+function BoundingBox(points)
+{
+	var minX = +Infinity, maxX = -Infinity, minY = +Infinity, maxY = -Infinity;
+	for (var i = 0; i < points.length; i++) {
+		minX = Math.min(minX, points[i].X);
+		minY = Math.min(minY, points[i].Y);
+		maxX = Math.max(maxX, points[i].X);
+		maxY = Math.max(maxY, points[i].Y);
+	}
+	return new Rectangle(minX, minY, maxX - minX, maxY - minY);
+}
+function PathDistance(pts1, pts2)
+{
+	var d = 0.0;
+	for (var i = 0; i < pts1.length; i++) // assumes pts1.length == pts2.length
+		d += Distance(pts1[i], pts2[i]);
+	return d / pts1.length;
+}
+function PathLength(points)
+{
+	var d = 0.0;
+	for (var i = 1; i < points.length; i++)
+		d += Distance(points[i - 1], points[i]);
+	return d;
+}
+function Distance(p1, p2)
+{
+	var dx = p2.X - p1.X;
+	var dy = p2.Y - p1.Y;
+	return Math.sqrt(dx * dx + dy * dy);
+}
+function Deg2Rad(d) { return (d * Math.PI / 180.0); }
